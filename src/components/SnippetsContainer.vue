1499675f663190ca581139f708a51e6381969b26
Valerie Eskens <valerie.eskens@kbc.be>
2018-01-25 16:07:24 +0100

    SnippetsContainer + Highlights component

---
+++ src/components/SnippetsContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="snippets">
+    <Highlight
+      v-for="(highlight, key) in highlights"
+      :key="key+10"
+      :content="highlight.highlight"
+      :source="highlight.source"
+      :author="highlight.author"
+    />
+  </div>
+</template>
+
+<script>
+import Highlight from '@/components/snippets/Highlight.vue'
+import highlights from '@/data/highlights.json'
+
+export default {
+  name: 'SnippetsContainer',
+  components: { Highlight },
+  data () {
+    return {
+      highlights: highlights
+    }
+  }
+}
+</script>
---
+++ src/components/snippets/Highlight.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="highlight" @click="goToSource()">
+      <div v-html="content" class="highlight__content"></div>
+      <div class="highlight__author">{{ author }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Highight',
+  props: {
+    content: String,
+    source: String,
+    author: String
+  },
+  methods: {
+    goToSource () {
+      window.open(this.source)
+    }
+  }
+}
+</script>
