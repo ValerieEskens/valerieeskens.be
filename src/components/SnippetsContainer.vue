a07c129d271cc09d649476e32a13f6b12e1d3caa
Valerie Eskens <valerieeskens@gmail.com>
2018-01-26 23:50:15 +0100

    random colors

--- src/components/SnippetsContainer.vue
+++ src/components/SnippetsContainer.vue
@@ -2,6 +2,7 @@
   <div class="snippets">
     <Highlight
       class="snippet"
+      :style="{ 'box-shadow': `2px 2px 3px 1px ${highlight.color}, inset 0 0 5px 1px ${highlight.color}` }"
       v-for="(highlight, key) in highlights"
       :key="key+10"
       :content="highlight.highlight"
@@ -20,8 +21,20 @@ export default {
   components: { Highlight },
   data () {
     return {
-      highlights: highlights
+      highlights: highlights,
+      colors: ['#F4BE75', '#CC787F', '#A36E81', '#655C77', '#4A5B78']
     }
+  },
+  created () {
+    this.highlights.forEach(highlight => {
+      highlight.color = this.getColor();
+    });
+  },
+  methods: {
+    getColor () {
+      return this.colors[Math.floor(Math.random() * this.colors.length)]
+    },
+
   }
 }
 </script>
