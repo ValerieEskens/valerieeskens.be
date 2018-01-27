e2d40ccedd1b75e49e324ad54fd15d3f1d6cb254
Valerie Eskens <valerieeskens@gmail.com>
2018-01-27 21:01:50 +0100

    articles setup

--- src/components/SnippetsContainer.vue
+++ src/components/SnippetsContainer.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="snippets">
+    <Article
+      class="snippet"
+      v-for="(article, key) in articles"
+      :key="key+10"
+      :image="article.image"
+      :source="article.source"
+      :author="article.author"
+    />
+
     <Highlight
       class="snippet"
       v-for="(highlight, key) in highlights"
-      :key="key+10"
+      :key="key"
       :content="highlight.highlight"
       :source="highlight.source"
       :author="highlight.author"
@@ -14,13 +23,16 @@
 <script>
 import Highlight from '@/components/snippets/Highlight.vue'
 import highlights from '@/data/highlights.json'
+import Article from '@/components/snippets/Article.vue'
+import articles from '@/data/articles.json'
 
 export default {
   name: 'SnippetsContainer',
-  components: { Highlight },
+  components: { Highlight, Article },
   data () {
     return {
-      highlights: highlights
+      highlights: highlights,
+      articles: articles
     }
   }
 }
